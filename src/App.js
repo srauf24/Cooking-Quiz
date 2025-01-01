3e19b67a3725c66f1cad908be9cea03a185074e8
Samee Rauf <samee.rauf@pmg.com>
2025-01-01 13:38:02 -0800

    added image and basic styling"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import butterChicken from './butter_chicken.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="Header">
-        <h1> On My Grind</h1>
+      <header className="App-header">
+        <img src={butterChicken} alt="Butter Chicken" className="ButterChicken" />
+        <h1>On My Grind</h1>
+        <p>So you think you can cook Pakistani food? Let's put that to the test...</p>
       </header>
     </div>
   );
