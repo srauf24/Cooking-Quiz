bae42541d641226bd9f0db51051d7b7f21b85cdc
Samee Rauf <samee.rauf@pmg.com>
2025-01-01 16:58:20 -0800

    added chai image back in

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import BaristaForm from './Components/BaristaForm';
 function App() {
   return (
     <div className="App">
-     <div className="title">
-         <h1>
-         On my grind
-        </h1>
-         <p> So you think you can barista? Let's put that to the test..</p>
-     </div>
-        <BaristaForm />
+        <div className="title">
+            <img src={chai} alt="Chai" className={"Chai"}/>
+
+            <h1>
+                On my grind
+            </h1>
+            <p> So you think you can barista? Let's put that to the test..</p>
+        </div>
+        <BaristaForm/>
     </div>
   );
 }
